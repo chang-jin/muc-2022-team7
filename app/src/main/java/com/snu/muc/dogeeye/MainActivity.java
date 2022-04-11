cb99d13f528bec26a44dd164fb464e09850afa85
BHKo <devko@kakao.com>
2022-04-12 02:08:20 +0900

    add GPS and step counter functions

--- app/src/main/java/com/snu/muc/dogeeye/MainActivity.java
+++ app/src/main/java/com/snu/muc/dogeeye/MainActivity.java
@@ -1,10 +1,35 @@
 package com.snu.muc.dogeeye;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,6 +40,55 @@ import com.snu.muc.dogeeye.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private static final int GPS_UTIL_LOCATION_PERMISSION_REQUEST_CODE = 100;
+    private static final int ACTIVITY_RECOGNITION_PERMISSION_REQUEST_CODE = 200;
+
+    private void checkLocationPermission() {
+        int accessLocation = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int accessActivity = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION);
+
+        if (accessLocation == PackageManager.PERMISSION_DENIED || accessActivity == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACTIVITY_RECOGNITION}, GPS_UTIL_LOCATION_PERMISSION_REQUEST_CODE);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == GPS_UTIL_LOCATION_PERMISSION_REQUEST_CODE) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (Manifest.permission.ACCESS_FINE_LOCATION.equals(permissions[i])) {
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setTitle("위치 권한이 꺼져있습니다.");
+                        builder.setMessage("[권한] 설정에서 위치 권한을 허용해야 합니다.");
+                        builder.setPositiveButton("설정으로 가기", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent();
+                                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                intent.setData(uri);
+                                startActivity(intent);
+                            }
+                        }).setNegativeButton("종료", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +106,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        checkLocationPermission();
     }
 
 }
--- app/src/main/java/com/snu/muc/dogeeye/ui/record/RecordFragment.java
+++ app/src/main/java/com/snu/muc/dogeeye/ui/record/RecordFragment.java
@@ -1,10 +1,21 @@
 package com.snu.muc.dogeeye.ui.record;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+
+import android.content.IntentSender;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,12 +23,107 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.snu.muc.dogeeye.MainActivity;
 import com.snu.muc.dogeeye.databinding.FragmentRecordBinding;
 
-public class RecordFragment extends Fragment {
+import java.util.concurrent.Executor;
 
+public class RecordFragment extends Fragment implements SensorEventListener {
+
+    private TextView stepView;
+    private TextView locView;
+    private Context mContext;
+    private Activity activity;
     private RecordViewModel recordViewModel;
     private FragmentRecordBinding binding;
+    private SensorManager sensorManager;
+    private Sensor stepCountSensor;
+
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+    private double longitude, latitude;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int GPS_UTIL_LOCATION_RESOLUTION_REQUEST_CODE = 101;
+
+    public static final int DEFAULT_LOCATION_REQUEST_PRIORITY = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY;
+    public static final long DEFAULT_LOCATION_REQUEST_INTERVAL = 20000L;
+    public static final long DEFAULT_LOCATION_REQUEST_FAST_INTERVAL = 10000L;
+
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            longitude = locationResult.getLastLocation().getLongitude();
+            latitude = locationResult.getLastLocation().getLatitude();
+            locView.setText(longitude + ", " + latitude);
+
+            Toast.makeText(getContext(),"LOC is called!", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+
+        if (context instanceof Activity)
+            activity = (Activity) context;
+    }
+
+    private void checkLocationSetting() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(DEFAULT_LOCATION_REQUEST_PRIORITY);
+        locationRequest.setInterval(DEFAULT_LOCATION_REQUEST_INTERVAL);
+        locationRequest.setFastestInterval(DEFAULT_LOCATION_REQUEST_FAST_INTERVAL);
+
+        SettingsClient settingsClient = LocationServices.getSettingsClient(activity);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest).setAlwaysShow(true);
+        settingsClient.checkLocationSettings(builder.build())
+                .addOnSuccessListener(getActivity(), new OnSuccessListener<LocationSettingsResponse>() {
+                    @SuppressLint("MissingPermission")
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+                    }
+                })
+                .addOnFailureListener(getActivity(), new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                try {
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(getActivity(), GPS_UTIL_LOCATION_RESOLUTION_REQUEST_CODE);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.w(TAG, "unable to start resolution for result due to " + sie.getLocalizedMessage());
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "location settings are inadequate, and cannot be fixed here. Fix in Settings.";
+                                Log.e(TAG, errorMessage);
+                        }
+                    }
+                });
+    }
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +133,16 @@ public class RecordFragment extends Fragment {
         binding = FragmentRecordBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (stepCountSensor == null) {
+            Toast.makeText(getContext(), "No Step Sensor", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            sensorManager.registerListener(this,stepCountSensor,SensorManager.SENSOR_DELAY_FASTEST);
+        }
+        stepView = binding.stepView;
+        locView = binding.locView;
         final TextView textView = binding.textDashboard;
         recordViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -34,6 +150,8 @@ public class RecordFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        checkLocationSetting();
         return root;
     }
 
@@ -41,5 +159,20 @@ public class RecordFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER){
+
+            stepView.setText("Step : " + sensorEvent.values[0]);
+
+            Toast.makeText(getContext(),"Sensor Called!",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
