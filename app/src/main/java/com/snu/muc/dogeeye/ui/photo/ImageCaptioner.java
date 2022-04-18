3ff7c92d8762425027a03a655275a1f51df5b917
Minjae Kim <widiba03304@gmail.com>
2022-04-19 08:36:06 +0900

    add image captioning feature

---
+++ app/src/main/java/com/snu/muc/dogeeye/ui/photo/ImageCaptioner.java
@@ -0,0 +1,192 @@
+package com.snu.muc.dogeeye.ui.photo;
+
+import static java.lang.Math.min;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.Tensor;
+import org.tensorflow.lite.gpu.CompatibilityList;
+import org.tensorflow.lite.gpu.GpuDelegate;
+import org.tensorflow.lite.nnapi.NnApiDelegate;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorOperator;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
+import org.tensorflow.lite.support.image.ops.Rot90Op;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public abstract class ImageCaptioner {
+    public static final String TAG = "ImageCaptionerWithSupport";
+    private static String[] WORD_MAP;
+
+    public enum Model {
+        RESNET_RNN,
+        MOBILENET_GRU
+    }
+
+    public enum Device {
+        CPU,
+        GPU,
+        NNAPI
+    }
+
+    private final int imageSizeX;
+    private final int imageSizeY;
+
+    private GpuDelegate gpuDelegate = null;
+    private NnApiDelegate nnApiDelegate = null;
+
+    protected Interpreter tflite;
+
+    private final Interpreter.Options tfliteOptions = new Interpreter.Options();
+
+    private TensorImage inputImageBuffer;
+    private int[][] outputCaptionIds = new int[22][1];
+    Map<Integer, Object> outputBuffers = new HashMap<>();
+
+    public static ImageCaptioner create(Activity activity, Model model, Device device, int numThreads)
+            throws IOException {
+        // Load word map file
+        InputStream is = activity.getAssets().open("idmap");
+        BufferedReader r = new BufferedReader(new InputStreamReader(is));
+        StringBuilder total = new StringBuilder();
+        String line;
+        try {
+            while ((line = r.readLine()) != null) {
+                total.append(line).append('\n');
+            }
+        } catch (IOException e) {
+             Log.d(TAG, "Cannot read word map file!");
+        }
+        WORD_MAP = total.toString().split("\n");
+
+        if (model == Model.RESNET_RNN) {
+            return new ResnetRnnImageCaptioner(activity, device, numThreads);
+        } else if (model == Model.MOBILENET_GRU) {
+            return new MobilenetGruImageCaptioner(activity, device, numThreads);
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    protected ImageCaptioner(Activity activity, Device device, int numThreads) throws IOException {
+        MappedByteBuffer tfliteModel = FileUtil.loadMappedFile(activity, getModelPath());
+        if (device == Device.NNAPI) {
+            nnApiDelegate = new NnApiDelegate();
+            tfliteOptions.addDelegate(nnApiDelegate);
+        } else if (device == Device.GPU) {
+            CompatibilityList compatList = new CompatibilityList();
+            if (compatList.isDelegateSupportedOnThisDevice()) {
+                GpuDelegate.Options delegateOptions = compatList.getBestOptionsForThisDevice();
+                GpuDelegate gpuDelegate = new GpuDelegate(delegateOptions);
+                tfliteOptions.addDelegate(gpuDelegate);
+            } else {
+                tfliteOptions.setUseXNNPACK(true);
+            }
+        } else if (device == Device.CPU) {
+            tfliteOptions.setUseXNNPACK(true);
+        }
+
+        tfliteOptions.setNumThreads(numThreads);
+        tflite = new Interpreter(tfliteModel, tfliteOptions);
+
+        int imageTensorIndex = 0;
+        Tensor imageTensor = tflite.getInputTensor(imageTensorIndex);
+        int[] imageShape = imageTensor.shape();
+        Log.d(TAG, "Image shape: " + String.valueOf(imageShape[0]));
+        Log.d(TAG, String.valueOf(imageShape[1]));
+        Log.d(TAG, String.valueOf(imageShape[2]));
+        Log.d(TAG, String.valueOf(imageShape[3]));
+        imageSizeX = imageShape[1]; // 224
+        imageSizeY = imageShape[2]; // 224
+        DataType imageDataType = imageTensor.dataType();
+        inputImageBuffer = new TensorImage(imageDataType);
+
+        for (int captionTensorIndex = 0; captionTensorIndex < 22; captionTensorIndex++) {
+            outputBuffers.put(captionTensorIndex, outputCaptionIds[captionTensorIndex]);
+        }
+    }
+
+    protected abstract String getModelPath();
+
+    public void captionImage(final Bitmap bitmap, int sensorOrientation) {
+        long startTimeForLoadImage = SystemClock.uptimeMillis();
+        inputImageBuffer = loadImage(bitmap, sensorOrientation);
+        long endTimeForLoadImage = SystemClock.uptimeMillis();
+        Log.d(TAG, "Image Load Time: " + (endTimeForLoadImage - startTimeForLoadImage));
+
+        long startTimeForInference = SystemClock.uptimeMillis();
+        ByteBuffer[] inputBuffers = { inputImageBuffer.getBuffer() };
+        tflite.runForMultipleInputsOutputs(inputBuffers, outputBuffers);
+        long endTimeForInference = SystemClock.uptimeMillis();
+        Log.d(TAG, "Inference Time: " + (endTimeForInference - startTimeForInference));
+
+        String resultString = "";
+        for (int i = 0; i < 22; i++) {
+            resultString += WORD_MAP[outputCaptionIds[i][0]] + " ";
+        }
+        Log.d(TAG, resultString);
+    }
+
+    public void close() {
+        if (tflite != null) {
+            tflite.close();
+            tflite = null;
+        }
+        if (gpuDelegate != null) {
+            gpuDelegate.close();
+            gpuDelegate = null;
+        }
+        if (nnApiDelegate != null) {
+            nnApiDelegate.close();
+            nnApiDelegate = null;
+        }
+    }
+
+    public int getImageSizeX() {
+        return imageSizeX;
+    }
+
+    public int getImageSizeY() {
+        return imageSizeY;
+    }
+
+    private TensorImage loadImage(final Bitmap bitmap, int sensorOrientation) {
+        inputImageBuffer.load(bitmap);
+
+        int cropSize = min(bitmap.getWidth(), bitmap.getHeight());
+        Log.d(TAG, String.valueOf(bitmap.getWidth()));
+        Log.d(TAG, String.valueOf(bitmap.getHeight()));
+        Log.d(TAG, String.valueOf(imageSizeX));
+        Log.d(TAG, String.valueOf(imageSizeY));
+        int numRotation = sensorOrientation / 90;
+
+        ImageProcessor imageProcessor = new ImageProcessor.Builder()
+                .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+                .add(new Rot90Op(numRotation))
+                .add(getPreprocessNormalizeOp())
+                .build();
+        return imageProcessor.process(inputImageBuffer);
+    }
+
+    protected abstract TensorOperator getPreprocessNormalizeOp();
+}
--- app/src/main/java/com/snu/muc/dogeeye/ui/photo/PhotoFragment.java
+++ app/src/main/java/com/snu/muc/dogeeye/ui/photo/PhotoFragment.java
@@ -2,8 +2,12 @@ package com.snu.muc.dogeeye.ui.photo;
 
 import static java.lang.Thread.sleep;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,25 +24,41 @@ import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import com.snu.muc.dogeeye.databinding.FragmentPhotoBinding;
 
+import com.snu.muc.dogeeye.ui.photo.ImageCaptioner.Device;
+import com.snu.muc.dogeeye.ui.photo.ImageCaptioner.Model;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
 public class PhotoFragment extends Fragment {
+    private final String TAG = "PhotoFragment";
 
+    private ImageCaptioner imageCaptioner;
     private PhotoViewModel photoViewModel;
     private FragmentPhotoBinding binding;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private int imageSizeX;
+    private int imageSizeY;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        // create tflite image captioner
+        createImageCaptioner(Model.MOBILENET_GRU, Device.CPU, 1);
+        if (imageCaptioner == null) {
+            Log.d(TAG, "Failed to load image captioner.");
+        } else {
+            Log.d(TAG, "Success to load image captioner.");
+        }
 
+        // create view model for photo view
         photoViewModel =
                 new ViewModelProvider(this).get(PhotoViewModel.class);
         cameraProviderFuture = ProcessCameraProvider.getInstance(getContext());
@@ -64,13 +84,27 @@ public class PhotoFragment extends Fragment {
         return root;
     }
 
+    private void createImageCaptioner(Model model, Device device, int numThreads) {
+        if (imageCaptioner != null) {
+            return;
+        }
+        try {
+            imageCaptioner = ImageCaptioner.create(getActivity(), model, device, numThreads);
+        } catch (IOException | IllegalArgumentException e) {
+            Log.e(TAG, e.toString());
+        }
+
+        imageSizeX = imageCaptioner.getImageSizeX();
+        imageSizeY = imageCaptioner.getImageSizeY();
+    }
+
     void bindPreview(@NonNull ProcessCameraProvider cameraProvider,
                      @NonNull CameraSelector cameraSelector) {
         final PreviewView previewView = binding.previewView;
         final ImageAnalysis imageAnalysis =
                 new ImageAnalysis.Builder()
                         .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
-                        .setTargetResolution(new Size(1280, 720))
+                        .setTargetResolution(new Size(224, 224))
                         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                         .build();
 
@@ -78,15 +112,23 @@ public class PhotoFragment extends Fragment {
             @Override
             public void analyze(@NonNull ImageProxy imageProxy) {
                 int rotationDegrees = imageProxy.getImageInfo().getRotationDegrees();
-                Image image = imageProxy.getImage();
-
-                try {
-                    sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                // image is JPEG, so need to decode to obtain bitmap
+                ImageProxy.PlaneProxy plane = imageProxy.getPlanes()[0];
+                ByteBuffer byteBuffer = imageProxy.getPlanes()[0].getBuffer();
+                int pixelStride = plane.getPixelStride();
+                int rowStride = plane.getRowStride();
+                int rowPadding = rowStride - pixelStride * imageProxy.getWidth();
+                Bitmap bitmap = Bitmap.createBitmap(
+                        imageProxy.getWidth() + rowPadding / pixelStride,
+                        imageProxy.getHeight(),
+                        Bitmap.Config.ARGB_8888);
+                bitmap.copyPixelsFromBuffer(byteBuffer);
+                if (bitmap == null) {
+                    Log.d(TAG, "bitmap is null!");
+                } else {
+                    imageCaptioner.captionImage(bitmap, rotationDegrees);
+                    photoViewModel.postText(String.valueOf(rotationDegrees));
                 }
-                // Call tensorflow lite inference
-                photoViewModel.postText(String.valueOf(rotationDegrees));
                 imageProxy.close();
             }
         });
