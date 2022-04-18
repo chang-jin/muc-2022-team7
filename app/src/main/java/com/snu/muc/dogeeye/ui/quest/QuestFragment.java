01a90dc76083f6f1d5cde4637dc1a869a3a46745
Changjin Jeong <creatively.jeong@gmail.com>
2022-04-18 16:07:48 +0900

    Added some button and modify ui

--- app/src/main/java/com/snu/muc/dogeeye/ui/quest/QuestFragment.java
+++ app/src/main/java/com/snu/muc/dogeeye/ui/quest/QuestFragment.java
@@ -1,12 +1,14 @@
 package com.snu.muc.dogeeye.ui.quest;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,98 +25,79 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.snu.muc.dogeeye.R;
+import com.snu.muc.dogeeye.common.Logger;
 import com.snu.muc.dogeeye.databinding.FragmentQuestBinding;
 
 public class QuestFragment extends Fragment {
 
+    private static final Logger log = new Logger();
     private static final int RC_LEADERBOARD_UI = 9004;
     private Activity mActivity;
-    private QuestViewModel questViewModel;
     private FragmentQuestBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        questViewModel =
-                new ViewModelProvider(this).get(QuestViewModel.class);
         mActivity = getActivity();
 
         binding = FragmentQuestBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        questViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        final Button leaderBoard = binding.leaderboardBtn;
+        leaderBoard.setOnClickListener(view -> displayLeaderboard());
         checkUserSignIn();
 
         return root;
     }
 
     private void displayLeaderboard() {
-        Log.d("Changjin", "displayLeaderBoard");
-
+        log.d("displayLeaderBoard");
         try {
             PlayGames.getLeaderboardsClient(mActivity)
                     .getLeaderboardIntent(getString(R.string.leaderboard_id))
-                    .addOnSuccessListener(new OnSuccessListener<Intent>() {
-                        @Override
-                        public void onSuccess(Intent intent) {
-                            Log.d("Changjin", "onSuccess");
-                            startActivityForResult(intent, RC_LEADERBOARD_UI);
-                        }
+                    .addOnSuccessListener(intent -> {
+                        log.d("onSuccess");
+                        startActivityForResult(intent, RC_LEADERBOARD_UI);
                     })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d("Changjin", "onFailure : " + e);
-                            Log.d("Changjin", "onFailure : " + e.getLocalizedMessage());
-                            Log.d("Changjin", "onFailure : " + e.toString());
-                            Log.d("Changjin", "onFailure : " + e.getStackTrace());
-                        }
-                    });
+                    .addOnFailureListener(e -> log.d("onFailure"));
         } catch (Exception e) {
-            Log.d("Changjin", "Error = " + e.getMessage());
+            log.e("Error = " + e.getMessage());
         }
     }
-
 
     private void checkUserSignIn() {
         GamesSignInClient gamesSignInClient = PlayGames.getGamesSignInClient(mActivity);
 
         gamesSignInClient.isAuthenticated().addOnCompleteListener(isAuthenticatedTask -> {
-            Log.d("Changjin", "isAuthenticated : " + isAuthenticatedTask.isSuccessful());
-            Log.d("Changjin", "isAuthenticated2 : " + isAuthenticatedTask.getResult().isAuthenticated());
             boolean isAuthenticated =
                     (isAuthenticatedTask.isSuccessful() &&
                             isAuthenticatedTask.getResult().isAuthenticated());
 
             if (isAuthenticated) {
                 // Continue with Play Games Services
-                Log.d("Changjin", "isAuthenticated");
+                binding.leaderboardBtn.setVisibility(View.VISIBLE);
             } else {
-                // Disable your integration with Play Games Services or show a
-                // login button to ask  players to sign-in. Clicking it should
-                // call GamesSignInClient.signIn().
-                Log.d("Changjin", "signIn");
-                gamesSignInClient.signIn();
-
+                binding.signInBtn.setOnClickListener(view -> gamesSignInClient.signIn());
+                binding.signInBtn.setVisibility(View.VISIBLE);
             }
         });
-        PlayGames.getPlayersClient(mActivity).getCurrentPlayer().addOnCompleteListener(task -> {
+        PlayGames.getPlayersClient(mActivity).getCurrentPlayer().
+                addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        Log.d("Changjin", "signIn done : " + task.getResult().getPlayerId());
-                        displayLeaderboard();
+                        binding.welcome.setText(task.getResult().getDisplayName() + " Welcome!");
+                        binding.leaderboardBtn.setVisibility(View.VISIBLE);
+//                        submitScoreToLeaderboard();
                     }
                 }
         );
     }
 
     private void submitScoreToLeaderboard() {
-        PlayGames.getLeaderboardsClient(getActivity())
-                .submitScore(getString(R.string.leaderboard_id), 1337);
+        PlayGames.getLeaderboardsClient(mActivity)
+                .submitScore(getString(R.string.leaderboard_id), 80);
+        PlayGames.getLeaderboardsClient(mActivity)
+                .submitScore(getString(R.string.leaderboard_id), 50);
+        PlayGames.getLeaderboardsClient(mActivity)
+                .submitScore(getString(R.string.leaderboard_id), 100);
     }
 
     @Override
